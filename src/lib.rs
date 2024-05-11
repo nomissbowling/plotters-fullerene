0ac32d9efc17fc79e1a945f053bae789e266d803
nomissbowling <nomissbowling@yahoo.co.jp>
2024-05-11 15:25:11 +0900

    version 0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/plotters-fullerene/0.0.1")]
+#![doc(html_root_url = "https://docs.rs/plotters-fullerene/0.1.1")]
 //! plotters fullerene and polyhedron for Rust
 //!
 
